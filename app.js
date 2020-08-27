e7ae11d2d38c2eb8b3ae589913c990e666f9455e
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-08-26 20:16:20 -0700

    update

--- app.js
+++ app.js
@@ -11,12 +11,48 @@ mongoose.connect('mongodb://localhost/auth_demo', { useNewUrlParser: true, useUn
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+
+// PASSPORT ==================================================================
+
+app.use(require('express-session')({
+    secret: '2020',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser()); // Encoding session
+passport.deserializeUser(User.deserializeUser()); // Decoding session
+
+// ROUTES =====================================================================
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/secret', (req, res) => {
     res.render('secret')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+        if(err) {
+            console.log(err);
+            return res.render('register')
+            
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secret');
+            });
+        }
+    });
 });
 
 app.listen(3000, () => {
